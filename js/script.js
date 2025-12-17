6c66f4a11b71bcf58520f07d452c4fffc08cf304
fazllyrabby <fazleyrabby76@gmail.com>
2025-12-18 04:03:32 +0600

    design correction and info update with bug fixing about transition

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ toggleIcon.addEventListener('click',() => {
     toggleIcon.classList.toggle('bx-sun');
     document.body.classList.toggle('dark-mode');
 });
+// Select all navbar links
+const navLinks = document.querySelectorAll('.navbar a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        // Remove 'active' from all links
+        navLinks.forEach(nav => nav.classList.remove('active'));
+        // Add 'active' to the clicked link
+        this.classList.add('active');
+    });
+});
+
 let lastScrollTop = 0;
 const header = document.querySelector(".header");
 
