d51d62ca418b074b1ac5a2ca7d70e20c98a19bee
fazllyrabby <fazleyrabby76@gmail.com>
2025-12-18 02:31:18 +0600

    update about ,skills,projects,contact section

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ toggleIcon.addEventListener('click',() => {
     toggleIcon.classList.toggle('bx-sun');
     document.body.classList.toggle('dark-mode');
 });
+let lastScrollTop = 0;
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // scrolling down
+        header.classList.add("hide");
+    } else {
+        // scrolling up
+        header.classList.remove("hide");
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // avoid negative values
+}, false);
